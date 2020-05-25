4653d59c09535ac35c3cbe038b3f1572f86e6ce0
thtkiet19 <63939319+thtkiet19@users.noreply.github.com>
2020-05-25 20:34:52 +0700

    add test files and minor changes

--- AcademicStaffClass.h
+++ AcademicStaffClass.h
@@ -10,10 +10,18 @@ using namespace std;
 struct stu
 {
     int No, ID;
-    char lastname[50], firstname[50], gender[6], clas[10], username[10], password[10];
+    char fullname[100], gender[6], clas[10], username[10], password[10];
     int  year, month, day;
     stu* pNext;
 };
-
+void importstudent(ifstream& f, stu* student);
+void savedata(stu* student, ofstream& f);
+void changeclass(stu*& student, ofstream& f, char name[]);
+void editstudent(stu* student, ofstream& f, char name[]);
+void Int_to_Char(int n, char a[]);
+void AddAStudent(stu*& student, ofstream& f);
+void RemoveStudent(stu*& student, ofstream& f);
+void viewlistofclass();
+void viewlistofstudent(stu* student);
 
 #endif
