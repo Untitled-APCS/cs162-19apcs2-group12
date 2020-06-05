33855b80c436db96b8e3a7009f00fa55d92e8d5a
thtkiet19 <63939319+thtkiet19@users.noreply.github.com>
2020-06-05 17:44:31 +0700

    comment

--- AcademicStaffScoreBoard.h
+++ AcademicStaffScoreBoard.h
@@ -1,37 +1,85 @@
-#pragma once
-#ifndef __AcademicStaffScoreBoard__
-#define __AcademicStaffScoreBoard__
-#include <iostream>
-#include <string>
-#include <fstream>
-#include "Lecturer.h"
+//#pragma once
+//#ifndef __AcademicStaffScoreBoard__
+//#define __AcademicStaffScoreBoard__
+//#include <iostream>
+//#include <string>
+//#include <fstream>
+//#include "Lecturer.h"
+//
+//using namespace std;
+//struct BoardNode {
+//	string no;
+//	string student_id;
+//	string id;
+//	string fullname;
+//	string class_;
+//	string grade[4];// midterm->final->bonus->total
+//	BoardNode* next = nullptr;
+//};
+////**** Khang's part: ****
+//void import_from_csv(BoardNode*& head, ifstream& fin,StudentNode* root, ofstream &fout, string& coursename);
+//void update_grade(BoardNode* head, StudentNode* root);
+//void delete_all(BoardNode*& head);
+//
+//// Activities:
+//void view_scoreboard(string courseid);
+//void edit_grade(string courseid);
+//void import_scoreboard(string courseid);
+//
+//// Options:
+//void lecturer_options();
+//void scoreboard_options();
+//
+////Kiet's part
+//void outputScoreboard(BoardNode* pHead);
+//void exportscoreboard(BoardNode* pHead, ofstream& f);
+//
+//#endif
 
-using namespace std;
-struct BoardNode {
-	string no;
-	string student_id;
-	string id;
-	string fullname;
-	string class_;
-	string grade[4];// midterm->final->bonus->total
-	BoardNode* next = nullptr;
-};
-//**** Khang's part: ****
-void import_from_csv(BoardNode*& head, ifstream& fin,StudentNode* root, ofstream &fout, string& coursename);
-void update_grade(BoardNode* head, StudentNode* root);
-void delete_all(BoardNode*& head);
+void staff_3_1();
 
-// Activities:
-void view_scoreboard(string courseid);
-void edit_grade(string courseid);
-void import_scoreboard(string courseid);
+void staff_3_2();
 
-// Options:
-void lecturer_options();
-void scoreboard_options();
+void staff_3_3();
 
-//Kiet's part
-void outputScoreboard(BoardNode* pHead);
-void exportscoreboard(BoardNode* pHead, ofstream& f);
+void staff_3_4();
 
-#endif
+void staff_3_5();
+
+void staff_3_6();
+
+void staff_3_7();
+
+void staff_3_8();
+
+void staff_3_9();
+
+void staff_3_10();
+
+void staff_3_11();
+
+void staff_3_12();
+
+bool checkStaff_3_1();
+
+bool checkStaff_3_2();
+
+bool checkStaff_3_3();
+
+bool checkStaff_3_4();
+
+bool checkStaff_3_5();
+
+bool checkStaff_3_6();
+
+bool checkStaff_3_7();
+
+bool checkStaff_3_8();
+
+bool checkStaff_3_9();
+
+bool checkStaff_3_10();
+
+bool checkStaff_3_11();
+
+bool checkStaff_3_12();
