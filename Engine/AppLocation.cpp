9945aafd59c60087d9e1ea96f655fae1a5728da1
hvu <hoangvu03081@gmail.com>
2020-06-06 12:00:13 +0700

    Fixed bugs

--- Engine/AppLocation.cpp
+++ Engine/AppLocation.cpp
@@ -6,4 +6,5 @@
 
 string getLocation() {
     return "/Users/ducanchu/Documents/Assignments/CS162/cs162-19apcs2-group12/cmake-build-debug/";
+    //return "E:/University Works/CS162/GitHub Project/cs162-19apcs2-group12/cmake-build-debug/";
 }
