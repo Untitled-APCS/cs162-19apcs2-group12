2283a4f621fde0335d272b573e32ca3354c2655d
thtkiet19 <63939319+thtkiet19@users.noreply.github.com>
2020-06-11 18:43:11 +0700

    debug

--- cs162_19apcs2_untitled.cpp
+++ cs162_19apcs2_untitled.cpp
@@ -29,7 +29,7 @@ int main(int argc, const char **argv) {
     //logo();
     //logInMenu();
     //lecturer_1_();
-    staff_2_1();
+    staff_5_1();
     //cout << getCheckInCode("2019-2020-HK2", "19ctt2", "cs162", 3);
 
     //string s;
