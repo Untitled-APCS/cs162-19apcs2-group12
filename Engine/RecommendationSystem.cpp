106e138a8aabf097f6123205f310aa8554c8a686
Chu Duc An <chuducan.dev@gmail.com>
2020-06-11 15:08:45 +0700

    RecommendationSystem.h finished InputStudent() finished InputData() finished logIn() modified

--- Engine/RecommendationSystem.cpp
+++ Engine/RecommendationSystem.cpp
@@ -6,18 +6,23 @@
 
 
 StringNode::StringNode() {
-
+    s = "";
+    weight = -1;
 }
 
 bool cmpStringNode(const StringNode &a, const StringNode &b) {
-    return false;
+    //if (b.s == "") return true;
+    //if (a.s == "") return b.s == "";
+
+    if (a.weight != b.weight) return a.weight > b.weight;
+    return a.s < b.s;
 }
 
 int getMin(StringNode *stringList, double weight) {
     int ans = -1;
     double minc = weight;
 
-    for (int i = 0; i<=10; i++)
+    for (int i = 1; i<=10; i++)
         if (stringList[i].s == "")
             return i;
         else if (stringList[i].weight < minc) {
@@ -28,46 +33,252 @@ int getMin(StringNode *stringList, double weight) {
     return ans;
 }
 
-void getRecStudent(string *Rec, string hint) {
+bool getRecStudent(string *Rec, string hint) {
     StringNode *stringList = new StringNode [11];
 
     StudentList studentList;
-    studentList.load();
+    if (!studentList.load())
+        EXITCODE_V(6, false);
+
     StudentNode *Tail = nullptr;
     int tmp;
     double weight;
+
+    StudentNode *exactNode = studentList.find(hint, ACTIVE);
+    if (exactNode != nullptr) {
+        stringList[0].s = hint;
+        stringList[0].weight = 1;
+    }
 
     for (int i = 0; i<studentList.cnt; i++) {
         //check the actual length of the list < cnt?
         Tail = (i == 0 ? studentList.Head : Tail->Next);
         if (Tail == nullptr)
-        EXITCODE(6)
+            EXITCODE_V(6, false)
+
+        if (Tail == exactNode || !Tail->active)
+            continue;
 
         weight = max(similarity(hint, Tail->studentID),
-                 max(similarity(hint, Tail->studentName),
-                 max(similarity(hint, Tail->studentID + Tail->studentName),
-                     similarity(hint, Tail->studentName + Tail->studentID))));
-        tmp = getMin(stringList, weight);
+                    max(similarity(hint, Tail->studentName),
+                        max(similarity(hint, Tail->studentID + Tail->studentName),
+                            similarity(hint, Tail->studentName + Tail->studentID))));
 
+        tmp = getMin(stringList, weight);
         if (tmp == -1) continue;
 
         stringList[tmp].weight = weight;
-        //stringList[tmp].s =
+        stringList[tmp].s = Tail->studentID;
+        //cout << stringList[tmp].s << " " << stringList[tmp].weight << endl;
     }
+
+    //check the actual length of the list > cnt?
+    if (Tail->Next != nullptr)
+        EXITCODE_V(6, false)
+
+    sort(stringList+1, stringList+11, cmpStringNode);
+
+    for (int i = 0; i<=10; i++)
+        Rec[i] = stringList[i].s;
+
+    return true;
 }
 
-void getRecClass(string *Rec, string hint) {
+bool getRecClass(string *Rec, string hint) {
+    StringNode *stringList = new StringNode [11];
 
+    ClassList classList;
+    if (!classList.load())
+    EXITCODE_V(6, false);
+
+    ClassNode *Tail = nullptr;
+    int tmp;
+    double weight;
+
+    ClassNode *exactNode = classList.find(hint, ACTIVE);
+    if (exactNode != nullptr) {
+        stringList[0].s = hint;
+        stringList[0].weight = 1;
+    }
+
+    for (int i = 0; i<classList.cnt; i++) {
+        //check the actual length of the list < cnt?
+        Tail = (i == 0 ? classList.Head : Tail->Next);
+        if (Tail == nullptr)
+        EXITCODE_V(6, false)
+
+        if (Tail == exactNode || !Tail->active)
+            continue;
+
+        weight = similarity(hint, Tail->classID);
+
+        tmp = getMin(stringList, weight);
+        if (tmp == -1) continue;
+
+        stringList[tmp].weight = weight;
+        stringList[tmp].s = Tail->classID;
+        //cout << stringList[tmp].s << " " << stringList[tmp].weight << endl;
+    }
+
+    //check the actual length of the list > cnt?
+    if (Tail->Next != nullptr)
+    EXITCODE_V(6, false)
+
+    sort(stringList+1, stringList+11, cmpStringNode);
+
+    for (int i = 0; i<=10; i++)
+        Rec[i] = stringList[i].s;
+
+    return true;
 }
 
-void getRecSemester(string *Rec, string hint) {
+bool getRecSemester(string *Rec, string hint) {
+    StringNode *stringList = new StringNode [11];
 
+    SemesterList semesterList;
+    if (!semesterList.load())
+    EXITCODE_V(6, false);
+
+    SemesterNode *Tail = nullptr;
+    int tmp;
+    double weight;
+
+    SemesterNode *exactNode = semesterList.find(hint, ACTIVE);
+    if (exactNode != nullptr) {
+        stringList[0].s = hint;
+        stringList[0].weight = 1;
+    }
+
+    for (int i = 0; i<semesterList.cnt; i++) {
+        //check the actual length of the list < cnt?
+        Tail = (i == 0 ? semesterList.Head : Tail->Next);
+        if (Tail == nullptr)
+        EXITCODE_V(6, false)
+
+        if (Tail == exactNode || !Tail->active)
+            continue;
+
+        weight = similarity(hint, Tail->semesterID);
+
+        tmp = getMin(stringList, weight);
+        if (tmp == -1) continue;
+
+        stringList[tmp].weight = weight;
+        stringList[tmp].s = Tail->semesterID;
+        //cout << stringList[tmp].s << " " << stringList[tmp].weight << endl;
+    }
+
+    //check the actual length of the list > cnt?
+    if (Tail->Next != nullptr)
+    EXITCODE_V(6, false)
+
+    sort(stringList+1, stringList+11, cmpStringNode);
+
+    for (int i = 0; i<=10; i++)
+        Rec[i] = stringList[i].s;
+
+    return true;
 }
 
-void getRecLecturer(string *Rec, string hint) {
+bool getRecLecturer(string *Rec, string hint) {
+    StringNode *stringList = new StringNode [11];
 
+    LecturerList lecturerList;
+    if (!lecturerList.load())
+    EXITCODE_V(6, false);
+
+    LecturerNode *Tail = nullptr;
+    int tmp;
+    double weight;
+
+    LecturerNode *exactNode = lecturerList.find(hint, ACTIVE);
+    if (exactNode != nullptr) {
+        stringList[0].s = hint;
+        stringList[0].weight = 1;
+    }
+
+    for (int i = 0; i<lecturerList.cnt; i++) {
+        //check the actual length of the list < cnt?
+        Tail = (i == 0 ? lecturerList.Head : Tail->Next);
+        if (Tail == nullptr)
+        EXITCODE_V(6, false)
+
+        if (Tail == exactNode || !Tail->active)
+            continue;
+
+        weight = max(similarity(hint, Tail->lecturerID),
+                     max(similarity(hint, Tail->lecturerName),
+                         max(similarity(hint, Tail->lecturerID + Tail->lecturerName),
+                             similarity(hint, Tail->lecturerName + Tail->lecturerID))));
+
+        tmp = getMin(stringList, weight);
+        if (tmp == -1) continue;
+
+        stringList[tmp].weight = weight;
+        stringList[tmp].s = Tail->lecturerID;
+        //cout << stringList[tmp].s << " " << stringList[tmp].weight << endl;
+    }
+
+    //check the actual length of the list > cnt?
+    if (Tail->Next != nullptr)
+    EXITCODE_V(6, false)
+
+    sort(stringList+1, stringList+11, cmpStringNode);
+
+    for (int i = 0; i<=10; i++)
+        Rec[i] = stringList[i].s;
+
+    return true;
 }
 
-void getRecCourse(string semesterID, string classID, string *Rec, string hint) {
+bool getRecCourse(string semesterID, string classID, string *Rec, string hint) {
+    StringNode *stringList = new StringNode [11];
 
+    CourseList courseList;
+    if (!courseList.load(semesterID, classID))
+    EXITCODE_V(6, false);
+
+    CourseNode *Tail = nullptr;
+    int tmp;
+    double weight;
+
+    CourseNode *exactNode = courseList.find(hint, ACTIVE);
+    if (exactNode != nullptr) {
+        stringList[0].s = hint;
+        stringList[0].weight = 1;
+    }
+
+    for (int i = 0; i<courseList.cnt; i++) {
+        //check the actual length of the list < cnt?
+        Tail = (i == 0 ? courseList.Head : Tail->Next);
+        if (Tail == nullptr)
+        EXITCODE_V(6, false)
+
+        if (Tail == exactNode || !Tail->active)
+            continue;
+
+        weight = max(similarity(hint, Tail->courseID),
+                     max(similarity(hint, Tail->courseName),
+                         max(similarity(hint, Tail->courseID + Tail->courseName),
+                             similarity(hint, Tail->courseName + Tail->courseID))));
+
+        tmp = getMin(stringList, weight);
+        if (tmp == -1) continue;
+
+        stringList[tmp].weight = weight;
+        stringList[tmp].s = Tail->courseID;
+        //cout << stringList[tmp].s << " " << stringList[tmp].weight << endl;
+    }
+
+    //check the actual length of the list > cnt?
+    if (Tail->Next != nullptr)
+    EXITCODE_V(6, false)
+
+    sort(stringList+1, stringList+11, cmpStringNode);
+
+    for (int i = 0; i<=10; i++)
+        Rec[i] = stringList[i].s;
+
+    return true;
 }
+
--- cs162_19apcs2_untitled.cpp
+++ cs162_19apcs2_untitled.cpp
@@ -7,13 +7,102 @@
 #include "Engine.h"
 #include <filesystem>
 
+bool checkTest(string* s, int cnt) {
+    for (int i = 0; i<cnt; i++)
+        for (int j = i+1; j<cnt; j++)
+            if (s[i] == s[j]) {
+                cout << "\n\nThree studentIDs must be nonidentical. [enter]\n";
+
+                fflush(stdin);
+                char keyPress = cin.get();
+                fflush(stdin);
+                return false;
+            }
+
+    return true;
+}
+
 int main(int argc, const char **argv) {
 //    fileLocation = argv[0];
 //    while (fileLocation.size() > 0 && fileLocation[fileLocation.size()-1] != '/')
 //        fileLocation.pop_back();
     //logo();
     //logInMenu();
-    lecturer_1_();
+    //lecturer_1_();
+
+    //string s;
+    //getline(cin, s);
+
+//    int cnt = 3;
+//    string *s = new string [cnt];
+//    fPtr *p = new fPtr [cnt] {inputStudent, inputStudent, inputStudent};
+//    if (!inputData(s, p, cnt, 0, checkTest))
+//        EXITCODE_V(6, 0)
+//
+//    for (int i = 0; i<cnt; i++)
+//        cout << s[i] << endl;
+
+    string ID;
+    int choice = 0;
+    char keyPress;
+
+    cout << "\n\nThe student with ID '" << ID << "' has already added.\n";
+    cout << "[1 + enter] Skip.\n";
+    cout << "[2 + enter] Replace.\n";
+    cout << "[3 + enter] Skip all.\n";
+    cout << "[4 + enter] Replace all.\n";
+
+    while (choice == 0) {
+        fflush(stdin);
+        keyPress = cin.get();
+        fflush(stdin);
+
+        switch (keyPress) {
+            case '1':
+                choice = SKIP;
+                break;
+            case '2':
+                choice = REPLACE;
+                break;
+            case '3':
+                choice = SKIP_ALL;
+                break;
+            case '4':
+                choice = REPLACE_ALL;
+                break;
+            default:
+                choice = 0;
+        }
+    }
+
+
+
+//    string *Rec = new string [11], hint = "dANG dINH";
+//    getRecStudent(Rec, hint);
+//
+//    StudentList studentList;
+//    StudentNode *studentNode;
+//    if (!studentList.load())
+//        EXITCODE_V(6, 0)
+//
+//    cout << "Hint: " << hint << endl;
+//    for (int i = 0; i<=10; i++)
+//    if (Rec[i] != "") {
+//        cout << i << " - " << Rec[i] << " - ";
+//
+//        studentNode = studentList.find(Rec[i], ACTIVE);
+//        if (studentNode == nullptr)
+//            EXITCODE_V(6, 0)
+//
+//        cout << studentNode->studentName << endl;
+//    }
+
+//    PathList pathList;
+//    string *hint = new string [1] {"19ctt"};
+//    pathList.getDirectoryList("/Users/ducanchu/Documents/Assignments/CS162/Standard CSV Input/CSVGenerator/cmake-build-debug/Student/", isStudentListCSV, hint, 1);
+//
+//    for (int i = 0; i<pathList.cnt; i++)
+//        cout << pathList.c[i].path << endl;
 
     //cout << getHashedPassword("ltathao");
 
